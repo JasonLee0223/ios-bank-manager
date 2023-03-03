401bb41fccded319f07d36a644c149a73047d3a4
Jason <ggj00125@gmail.com>
2023-03-03 19:42:52 +0900

    Feat: work(at identifier:) 메서드 추가 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Teller.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Teller.swift
@@ -15,12 +15,24 @@ final class Teller: Workable {
     }
     private(set) var customerQueue = Queue<Int>()
     
-    func working(responsibility: Int) {
-        customerQueue.euqueue(responsibility)
+    func working(responsibility: String, at orderNumber: Int) {
+        customerQueue.euqueue(orderNumber)
+        work(at: responsibility)
     }
     
     func finishing() -> Int? {
         guard let finishCustomNumber = customerQueue.dequeue() else { return nil }
         return finishCustomNumber
+    }
+    
+    private func work(at identifier: String) {
+        switch WorkType(rawValue: identifier) {
+        case .loan:
+            Thread.sleep(forTimeInterval: Requirement.LeadTime.loan)
+        case .deposit:
+            Thread.sleep(forTimeInterval: Requirement.LeadTime.deposit)
+        case .none:
+            print("sleep Error")
+        }
     }
 }
