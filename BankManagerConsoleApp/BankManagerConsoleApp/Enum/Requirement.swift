c6f2bd58ea1e4e7af16e6ed8b71bb4826fc5f349
Jason <ggj00125@gmail.com>
2023-03-07 16:35:22 +0900

    Refactor: WorkType에 CaseIterable 채택 및 leadTime 변수 구현
    - Requirement의 LeadTime 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/Enum/Requirement.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Enum/Requirement.swift
@@ -7,24 +7,26 @@
 
 import Foundation
 
-enum Requirement {
-    
-    enum CustomerCount {
-        static let defaultCustomer: Int = 1
-        static let minimum: Int = 10
-        static let maxmimum: Int = 30
-    }
-    enum LeadTime {
-        static let loan: Double = 1.1
-        static let deposit: Double = 0.7
-    }
+enum CustomerCount {
+    static let defaultCustomer: Int = 1
+    static let minimum: Int = 10
+    static let maxmimum: Int = 30
 }
 
-enum WorkType: String, CustomStringConvertible {
+enum WorkType: String, CustomStringConvertible, CaseIterable {
     case loan = "대출"
     case deposit = "예금"
     
     var description: String {
         return self.rawValue
+    }
+    
+    var leadTime: TimeInterval {
+        switch self {
+        case .deposit:
+            return 0.7
+        case .loan:
+            return 1.1
+        }
     }
 }
