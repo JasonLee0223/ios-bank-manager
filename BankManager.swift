17ff6a370430529f7a589a80052d75bc9bd7e2fe
Jason <ggj00125@gmail.com>
2023-03-07 16:43:00 +0900

    Refactor: 메서드 및 타입 분리로 인한 리펙터링

--- BankManager.swift
+++ BankManager.swift
@@ -7,7 +7,10 @@
 import Foundation
 
 struct BankManager {
-    static func startProcess() {
+    
+    //MARK: - Caller
+    
+    func startProcess() {
         var isContinue = true
         
         repeat {
@@ -33,118 +36,56 @@ struct BankManager {
         } while isContinue
     }
     
-    private static func beginWork() {
-        let tellers = assignTellers()
-        let totalCustomCount = generateRandomCustomerCount()
+    //MARK: - Callee
+    
+    private func beginWork() {
+        let generator = Generator()
+        let tellers = generator.assignTellers()
+        let totalCustomCount = generator.randomCustomerCount()
+        
+        let asyncProcess = AsyncProcess()
         var depositCustomCount = 0
         var loanCustomCount = 0
+        var isAllTaskFinish = false
         
-        
-        
-        let work = DispatchWorkItem {
-            for customNumber in Requirement.CustomerCount.defaultCustomer...totalCustomCount {
-                let order = customNumber % tellers.count
-                dispatchWorkQueue(teller: tellers[order], customNumber: customNumber)
-                
-                switch WorkType(rawValue: tellers[order].identifier) {
-                case .deposit:
-                    depositCustomCount += 1
-                case .loan:
-                    loanCustomCount += 1
-                case .none:
-                    print("Recognizer WorkType Error")
-                }
+        //MARK: - sync
+        for customerNumber in CustomerCount.defaultCustomer...totalCustomCount {
+            let order = customerNumber % tellers.count
+
+            //MARK: - async customer Queue
+            asyncProcess.customerProcess()
+
+            //MARK: - async teller Queue
+            let isFinishWork = asyncProcess.tellerProcess(teller: tellers[order], customerNumber)
+
+            switch tellers[order].identifier {
+            case .deposit:
+                depositCustomCount += 1
+            case .loan:
+                loanCustomCount += 1
             }
+            isAllTaskFinish = isFinishWork
         }
-        DispatchQueue.global().asyncAndWait(execute: work)
         
-        let totalSpend = calculateToLeadTimeBetween(depositCustomCount, and: loanCustomCount)
-        OutputMessage.todayWorkDeadline(customer: totalCustomCount, leadTime: totalSpend)
+        if isAllTaskFinish {
+            let totalSpend = calculateToLeadTimeBetween(depositCustomCount, and: loanCustomCount)
+            OutputMessage.todayWorkDeadline(customer: totalCustomCount, leadTime: totalSpend)
+        }
     }
 }
 
 //MARK: - Side Product
 extension BankManager {
-    private static func calculateToLeadTimeBetween(_ depositCustomCount: Int, and loanCustomCount: Int) -> String {
+    private func calculateToLeadTimeBetween(_ depositCustomCount: Int, and loanCustomCount: Int) -> String {
         let numberFormatter = NumberFormatter()
         numberFormatter.roundingMode = .halfUp
         numberFormatter.maximumSignificantDigits = 4
         
-        let totalLeadTime = Requirement.LeadTime.deposit * Double(depositCustomCount) + Requirement.LeadTime.loan * Double(loanCustomCount)
+        let totalLeadTime = WorkType.deposit.leadTime * Double(depositCustomCount) + WorkType.loan.leadTime * Double(loanCustomCount)
         
         guard let totalSpend = numberFormatter.string(for: totalLeadTime) else {
             return Errors.failOfFormatToString.localizedDescription
         }
-        
         return totalSpend
-    }
-    
-    private static func assignTellers() -> [Teller] {
-        let workTypeList = [WorkType.deposit, WorkType.deposit, WorkType.loan].shuffled()
-        var tellers = [Teller]()
-        
-        for index in 0...2 {
-            let workType = workTypeList[index]
-            tellers.append(Teller(identifier: workType.rawValue))
-        }
-        return tellers
-    }
-    
-    private static func determineIdentifier(work type: String) -> String {
-        switch WorkType(rawValue: type) {
-        case .loan:
-            return WorkType.loan.description
-        case .deposit:
-            return WorkType.deposit.description
-        default:
-            return "WorkType Error"
-        }
-    }
-    
-    private static func generateRandomCustomerCount() -> Int {
-        return Int.random(in: Requirement.CustomerCount.minimum...Requirement.CustomerCount.maxmimum)
-    }
-}
-
-//MARK: - DispatchQueue Method
-extension BankManager {
-    private static func dispatchWorkQueue(teller: Teller, customNumber: Int) {
-        let workQueue = DispatchQueue(label: "workQueue", attributes: .concurrent)
-        let workGroup = DispatchGroup()
-        let depositSemaphore = DispatchSemaphore(value: 2)
-        let loanSemaphore = DispatchSemaphore(value: 1)
-        
-        workGroup.enter()
-        switch WorkType(rawValue: teller.identifier) {
-        case .deposit:
-            workQueue.async {
-                depositSemaphore.wait()
-                asyncProcess(customNumber, teller)
-                depositSemaphore.signal()
-            }
-        case .loan:
-            workQueue.sync {
-                loanSemaphore.wait()
-                asyncProcess(customNumber, teller)
-                loanSemaphore.signal()
-            }
-        case .none:
-            print("WorkType Error")
-        }
-        workGroup.leave()
-    }
-    
-    private static func asyncProcess(_ customNumber: Int, _ teller: Teller) {
-        let bankProduct = determineIdentifier(work: teller.identifier)
-        
-        OutputMessage.work(start: customNumber, type: bankProduct)
-
-        teller.working(responsibility: bankProduct, at: customNumber)
-
-        guard let orderNumber = teller.finishing() else {
-            return
-        }
-
-        OutputMessage.work(finish: orderNumber, type: bankProduct)
     }
 }
