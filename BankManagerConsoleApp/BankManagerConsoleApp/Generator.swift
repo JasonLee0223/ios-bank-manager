76a5413853e95e540c2751565aa9cf6ac9195ea1
Jason <ggj00125@gmail.com>
2023-03-07 16:41:26 +0900

    Feat: Generator 구조체 타입 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Generator.swift
@@ -0,0 +1,25 @@
+//
+//  Generator.swift
+//  BankManagerConsoleApp
+//
+//  Created by Jason on 2023/03/07.
+//
+
+import Foundation
+
+struct Generator {
+    func assignTellers() -> [Teller] {
+        let workTypeList = [WorkType.deposit, WorkType.deposit, WorkType.loan].shuffled()
+        var tellers = [Teller]()
+        
+        for index in 0...2 {
+            let workType = workTypeList[index]
+            tellers.append(Teller(identifier: workType))
+        }
+        return tellers
+    }
+    
+    func randomCustomerCount() -> Int {
+        return Int.random(in: CustomerCount.minimum...CustomerCount.maxmimum)
+    }
+}
