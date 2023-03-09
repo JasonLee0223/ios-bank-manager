bd8836b9c3e85891fa15f85c79f1d951df24cd0e
Jason <ggj00125@gmail.com>
2023-03-10 02:18:11 +0900

    Feat: 필요한 UI 컴포넌트 틀 구현

--- BankManagerConsoleApp/BankManagerUIApp/View/MainView.swift
+++ BankManagerConsoleApp/BankManagerUIApp/View/MainView.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class MainView: UIView {
-
+    
+    //MARK: - initialize
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -18,4 +19,43 @@ class MainView: UIView {
         super.init(coder: coder)
         
     }
+    
+    //MARK: - UI Component
+    var stachView: UIStackView = {
+        let stackView = UIStackView()
+        
+        
+        return stackView
+    }()
+    
+    var addTenCustomer: UIButton = {
+        let addTenCustomer = UIButton()
+        
+        return addTenCustomer
+    }()
+    
+    var reset: UIButton = {
+        let reset = UIButton()
+        
+        return reset
+    }()
+    
+    var officeHours: UILabel = {
+        let officeHours = UILabel()
+        
+        
+        return officeHours
+    }()
+    
+    var waiting: UILabel = {
+        let waiting = UILabel()
+        
+        return waiting
+    }()
+    
+    var working: UILabel = {
+        let working = UILabel()
+        
+        return working
+    }()
 }
