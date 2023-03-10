6f2486e7f5f7b24124c1a8988a909d529cf29f56
Jason <ggj00125@gmail.com>
2023-03-10 16:10:35 +0900

    Feat: SubView 파일 생성 및 구현

---
+++ BankManagerConsoleApp/BankManagerUIApp/View/SubView.swift
@@ -0,0 +1,114 @@
+//
+//  SubView.swift
+//  BankManagerUIApp
+//
+//  Created by Jason on 2023/03/10.
+//
+
+import UIKit
+
+class SubView: UIView {
+    
+    //MARK: - initialize
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureLayout()
+        addLabels()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureLayout()
+        addLabels()
+    }
+    
+    //MARK: - View Component
+    var subStackView: UIStackView = {
+        let subStackView = UIStackView()
+        subStackView.axis = .horizontal
+        subStackView.distribution = .fillEqually
+        
+        return subStackView
+    }()
+    
+    var waitingScrollView: UIScrollView = {
+        let waitingScrollView = UIScrollView()
+        waitingScrollView.backgroundColor = .systemBlue
+        
+        return waitingScrollView
+    }()
+    
+    var workingScrollView: UIScrollView = {
+        let workingScrollView = UIScrollView()
+        workingScrollView.backgroundColor = .systemOrange
+        
+        return workingScrollView
+    }()
+    
+    var waitingStackView: UIStackView = {
+        let waitingStackView = UIStackView()
+        waitingStackView.axis = .horizontal
+        
+        return waitingStackView
+    }()
+    
+    var workingStackView: UIStackView = {
+        let workingStackView = UIStackView()
+        workingStackView.axis = .horizontal
+        
+        return workingStackView
+    }()
+    
+    //MARK: - UI Component
+    private func addLabels() {
+        (0..<5).map { index in
+            let label: UILabel = {
+                let label = UILabel()
+                label.text = "Test Text" + String(index)
+                label.textAlignment = .center
+                label.font = .systemFont(ofSize: 15, weight: .regular)
+                
+                return label
+            }()
+            return label
+        }
+        .forEach(waitingStackView.addArrangedSubview)
+    }
+    
+    //MARK: - Layout
+    private func configureLayout() {
+        self.addSubview(subStackView)
+        subStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            subStackView.topAnchor.constraint(equalTo: topAnchor),
+            subStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            subStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            subStackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        
+        [waitingScrollView, workingScrollView].forEach {
+            self.subStackView.addArrangedSubview($0)
+        }
+        
+        waitingScrollView.addSubview(waitingStackView)
+        workingScrollView.addSubview(workingStackView)
+        
+        waitingStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            waitingStackView.topAnchor.constraint(equalTo: waitingScrollView.topAnchor),
+            waitingStackView.leadingAnchor.constraint(equalTo: waitingScrollView.leadingAnchor),
+            waitingStackView.trailingAnchor.constraint(equalTo: waitingScrollView.trailingAnchor),
+            waitingStackView.bottomAnchor.constraint(equalTo: waitingScrollView.bottomAnchor),
+            waitingStackView.heightAnchor.constraint(equalTo: waitingScrollView.heightAnchor)
+        ])
+        
+        workingStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            workingStackView.topAnchor.constraint(equalTo: workingScrollView.topAnchor),
+            workingStackView.leadingAnchor.constraint(equalTo: workingScrollView.leadingAnchor),
+            workingStackView.trailingAnchor.constraint(equalTo: workingScrollView.trailingAnchor),
+            workingStackView.bottomAnchor.constraint(equalTo: workingScrollView.bottomAnchor),
+            workingStackView.heightAnchor.constraint(equalTo: workingScrollView.heightAnchor)
+        ])
+    }
+}
