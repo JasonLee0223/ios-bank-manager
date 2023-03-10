6ddf9b474b691c12bffb97fd1bfb89677563882d
Jason <ggj00125@gmail.com>
2023-03-10 14:31:32 +0900

    Feat: mainView AutoLayout 설정 완료

--- BankManagerConsoleApp/BankManagerUIApp/View/MainView.swift
+++ BankManagerConsoleApp/BankManagerUIApp/View/MainView.swift
@@ -24,80 +24,81 @@ class MainView: UIView {
     var mainStachView: UIStackView = {
         let mainStachView = UIStackView()
         mainStachView.axis = .vertical
-        mainStachView.spacing = 10
         mainStachView.distribution = .fillEqually
-        
-        //temp
-        mainStachView.backgroundColor = .darkGray
         
         return mainStachView
     }()
     
-//    var userInterfaceStackView: UIStackView = {
-//        let userInterfaceStackView = UIStackView()
-//        userInterfaceStackView.axis = .horizontal
-//        userInterfaceStackView.spacing = 10
-//        userInterfaceStackView.distribution = .fillEqually
-//
-//        //temp
-//        userInterfaceStackView.backgroundColor = .orange
-//
-//        return userInterfaceStackView
-//    }()
+    var userInterfaceStackView: UIStackView = {
+        let userInterfaceStackView = UIStackView()
+        userInterfaceStackView.axis = .horizontal
+        userInterfaceStackView.distribution = .fillEqually
+
+        return userInterfaceStackView
+    }()
     
-//    var statusStackView: UIStackView = {
-//        let statusStackView = UIStackView()
-//        statusStackView.axis = .horizontal
-//        statusStackView.spacing = 10
-//        statusStackView.distribution = .fillEqually
-//
-//        //temp
-//        statusStackView.backgroundColor = .lightGray
-//
-//        return statusStackView
-//    }()
-//
-//    var addTenCustomer: UIButton = {
-//        let addTenCustomer = UIButton()
-//        addTenCustomer.setTitle("고객 10명 추가", for: .normal)
-//        addTenCustomer.setTitleColor(.systemBlue, for: .normal)
-//
-//        return addTenCustomer
-//    }()
-//
-//    var reset: UIButton = {
-//        let reset = UIButton()
-//        reset.setTitle("초기화", for: .normal)
-//        reset.setTitleColor(.systemRed, for: .normal)
-//
-//        return reset
-//    }()
-//
-//    var officeHours: UILabel = {
-//        let officeHours = UILabel()
-//        officeHours.text = "업무시간"
-//        officeHours.font = .systemFont(ofSize: 20, weight: .regular)
-//
-//        return officeHours
-//    }()
-//
-//    var waiting: UILabel = {
-//        let waiting = UILabel()
-//        waiting.backgroundColor = .systemGreen
-//        waiting.text = "대기중"
-//        waiting.font = .systemFont(ofSize: 30, weight: .bold)
-//
-//        return waiting
-//    }()
-//
-//    var working: UILabel = {
-//        let working = UILabel()
-//        working.backgroundColor = .systemPurple
-//        working.text = "업무중"
-//        working.font = .systemFont(ofSize: 30, weight: .bold)
-//
-//        return working
-//    }()
+    var timerStackView: UIStackView = {
+        let timerStackView = UIStackView()
+        timerStackView.axis = .horizontal
+        timerStackView.distribution = .fill
+        
+        return timerStackView
+    }()
+    
+    var statusStackView: UIStackView = {
+        let statusStackView = UIStackView()
+        statusStackView.axis = .horizontal
+        statusStackView.distribution = .fillEqually
+
+        return statusStackView
+    }()
+
+    var addTenCustomer: UIButton = {
+        let addTenCustomer = UIButton()
+        addTenCustomer.setTitle("고객 10명 추가", for: .normal)
+        addTenCustomer.setTitleColor(.systemBlue, for: .normal)
+
+        return addTenCustomer
+    }()
+
+    var reset: UIButton = {
+        let reset = UIButton()
+        reset.setTitle("초기화", for: .normal)
+        reset.setTitleColor(.systemRed, for: .normal)
+
+        return reset
+    }()
+
+    var officeHours: UILabel = {
+        let officeHours = UILabel()
+        officeHours.text = "업무시간"
+        officeHours.font = .systemFont(ofSize: 20, weight: .regular)
+        officeHours.textAlignment = .center
+
+        return officeHours
+    }()
+
+    var waiting: UILabel = {
+        let waiting = UILabel()
+        waiting.backgroundColor = .systemGreen
+        waiting.text = "대기중"
+        waiting.textColor = .white
+        waiting.textAlignment = .center
+        waiting.font = .systemFont(ofSize: 30, weight: .bold)
+
+        return waiting
+    }()
+
+    var working: UILabel = {
+        let working = UILabel()
+        working.backgroundColor = .systemPurple
+        working.text = "업무중"
+        working.textColor = .white
+        working.textAlignment = .center
+        working.font = .systemFont(ofSize: 30, weight: .bold)
+
+        return working
+    }()
     
     //MARK: - Layout
     private func configurLayout() {
@@ -110,16 +111,16 @@ class MainView: UIView {
             mainStachView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
         
-//        self.mainStachView.addSubview(userInterfaceStackView)
-//
-//        self.mainStachView.addSubview(officeHours)
-//
-//        self.mainStachView.addSubview(statusStackView)
-//
-//        self.userInterfaceStackView.addArrangedSubview(addTenCustomer)
-//        self.userInterfaceStackView.addArrangedSubview(reset)
-//
-//        self.statusStackView.addArrangedSubview(waiting)
-//        self.statusStackView.addArrangedSubview(working)
+        [userInterfaceStackView, timerStackView, statusStackView].forEach{
+            self.mainStachView.addArrangedSubview($0)
+        }
+
+        self.userInterfaceStackView.addArrangedSubview(addTenCustomer)
+        self.userInterfaceStackView.addArrangedSubview(reset)
+        
+        self.timerStackView.addArrangedSubview(officeHours)
+
+        self.statusStackView.addArrangedSubview(waiting)
+        self.statusStackView.addArrangedSubview(working)
     }
 }
