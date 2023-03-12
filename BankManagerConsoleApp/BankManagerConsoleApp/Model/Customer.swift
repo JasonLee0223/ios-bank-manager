155c2b984be4d738095f955ca78aadf12ffa6bf4
Jason <ggj00125@gmail.com>
2023-03-12 15:44:22 +0900

    Feat: Customer 파일 재생성 및 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Customer.swift
@@ -0,0 +1,13 @@
+//
+//  Customer.swift
+//  BankManagerConsoleApp
+//
+//  Created by Jason on 2023/03/12.
+//
+
+import Foundation
+
+struct Customer {
+    let waitingNumber: Int
+    let workType: WorkType
+}
