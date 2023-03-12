6daf0077af4fffd225587d0753b847c46c5467cf
Jason <ggj00125@gmail.com>
2023-03-13 04:46:27 +0900

    Feat: SendCustomerInfoDelegate 파일 생성 및 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Protocol/SendCustomerInfoDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SendCustomerInfoDelegate.swift
+//  BankManagerConsoleApp
+//
+//  Created by Jason on 2023/03/13.
+//
+
+import Foundation
+
+protocol SendCustomerInfoDelegate: AnyObject {
+    func sendCustomerInfo()
+}
