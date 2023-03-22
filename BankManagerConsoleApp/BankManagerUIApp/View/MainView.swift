747aa18fcd1acfa705a2dffdc3cbc37477b9ba76
Jason <ggj00125@gmail.com>
2023-03-22 20:27:58 +0900

    Feat: UIAction 구현

--- BankManagerConsoleApp/BankManagerUIApp/View/MainView.swift
+++ BankManagerConsoleApp/BankManagerUIApp/View/MainView.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class MainView: UIView {
     
-    weak var delegate: SendCustomerInfoDelegate?
+    weak var mainViewdelegate: SendCustomerInfoDelegate?
     
     //MARK: - initialize
     override init(frame: CGRect) {
@@ -55,34 +55,55 @@ class MainView: UIView {
         
         return statusStackView
     }()
-
+    
+    var action: UIAction {
+        let action = UIAction { [self] _ in
+            guard let customers = mainViewdelegate?.sendCustomersInfo() else {
+                return
+            }
+            
+            mainViewdelegate?.makeCustomerLabel(customers: customers, completion: { [self] waitingCustomer in
+                
+                //MARK: - 업무 중 영역에 라벨이 생성된다.
+                mainViewdelegate?.drawinigWorkingLabel(of: waitingCustomer, completion: {
+                    
+                    //TODO: - officeHours Label이 업데이트 된다. (ex. 00:00:01)
+                    
+                    //TODO: - 업무 시간이 정해놓은 시간과 동일하면 완료 notify를 전달한다.
+                    
+                    //TODO: - 업무 시간만큼 지나면 waiting Label에서 해당 고객의 라벨이 제거되야한다.
+                    
+                })
+            })
+        }
+        return action
+    }
+    
     lazy var addTenCustomer: UIButton = {
-        let addTenCustomer = UIButton(type: .custom, primaryAction: UIAction(handler: {  _ in
-            self.delegate?.sendCustomerInfo()
-        }))
+        let addTenCustomer = UIButton(type: .custom, primaryAction: action)
         addTenCustomer.setTitle("고객 10명 추가", for: .normal)
         addTenCustomer.setTitleColor(.systemBlue, for: .normal)
-
+        
         return addTenCustomer
     }()
-
+    
     var reset: UIButton = {
         let reset = UIButton()
         reset.setTitle("초기화", for: .normal)
         reset.setTitleColor(.systemRed, for: .normal)
-
+        
         return reset
     }()
-
+    
     var officeHours: UILabel = {
         let officeHours = UILabel()
-        officeHours.text = "업무시간"
+        officeHours.text = "업무시간 - 00:00:00"
         officeHours.font = .systemFont(ofSize: 20, weight: .regular)
         officeHours.textAlignment = .center
-
+        
         return officeHours
     }()
-
+    
     var waiting: UILabel = {
         let waiting = UILabel()
         waiting.backgroundColor = .systemGreen
@@ -90,10 +111,10 @@ class MainView: UIView {
         waiting.textColor = .white
         waiting.textAlignment = .center
         waiting.font = .systemFont(ofSize: 30, weight: .bold)
-
+        
         return waiting
     }()
-
+    
     var working: UILabel = {
         let working = UILabel()
         working.backgroundColor = .systemPurple
@@ -101,7 +122,7 @@ class MainView: UIView {
         working.textColor = .white
         working.textAlignment = .center
         working.font = .systemFont(ofSize: 30, weight: .bold)
-
+        
         return working
     }()
     
@@ -119,12 +140,12 @@ class MainView: UIView {
         [userInterfaceStackView, timerStackView, statusStackView].forEach{
             self.mainStachView.addArrangedSubview($0)
         }
-
+        
         self.userInterfaceStackView.addArrangedSubview(addTenCustomer)
         self.userInterfaceStackView.addArrangedSubview(reset)
         
         self.timerStackView.addArrangedSubview(officeHours)
-
+        
         self.statusStackView.addArrangedSubview(waiting)
         self.statusStackView.addArrangedSubview(working)
     }
