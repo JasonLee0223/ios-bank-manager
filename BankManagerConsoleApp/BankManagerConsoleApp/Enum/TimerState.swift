007861e2ebe88b724f1c30ead8d2dd4b2c2dd2cb
Jason <ggj00125@gmail.com>
2023-03-22 21:22:24 +0900

    Feat: TimerState 타입 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Enum/TimerState.swift
@@ -0,0 +1,14 @@
+//
+//  TimerState.swift
+//  BankManagerConsoleApp
+//
+//  Created by Jason on 2023/03/22.
+//
+
+import Foundation
+
+enum TimerState {
+    case start
+    case suspended
+    case canceled
+}
